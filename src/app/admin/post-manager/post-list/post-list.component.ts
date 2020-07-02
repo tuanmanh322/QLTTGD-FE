eb29b57dac481d1dfb4c078733c8540fa8c83c8e
dangtuan096 <tuanmanh322@gmail.com>
2020-07-02 14:48:09 +0700

    done cate

--- src/app/admin/post-manager/post-list/post-list.component.ts
+++ src/app/admin/post-manager/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-post-list',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private title: Title
+  ) { }
 
   ngOnInit() {
+    this.title.setTitle('Quản lý bài viết');
   }
 
 }
