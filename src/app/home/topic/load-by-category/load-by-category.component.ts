d492e4f6ef71047f860294fdf024112e01f01ede
tuandm <tuandm@itsol.vn>
2020-08-05 22:55:55 +0700

    add cate

--- src/app/home/topic/load-by-category/load-by-category.component.ts
+++ src/app/home/topic/load-by-category/load-by-category.component.ts
@@ -10,6 +10,7 @@ import {Observable} from 'rxjs';
 import {BaiVietService} from '../../../shared/service/bai-viet.service';
 import {FormControl} from '@angular/forms';
 import {debounceTime, distinctUntilChanged, switchMap, tap} from 'rxjs/operators';
+import {TOKEN} from '../../../shared/model/qlttgd.constant';
 
 declare var $: any;
 
@@ -41,7 +42,16 @@ export class LoadByCategoryComponent implements OnInit {
   titleNew = '';
   bvTitle = new FormControl();
   idCD = 0;
-
+  dataIDBV = [0];
+  number1 = 0;
+  number2 = 0;
+  isActionLike: boolean;
+  isActionDisLike: boolean;
+  isLike: boolean;
+  isDislike: boolean;
+  disableLike: boolean;
+  disableDislike: boolean;
+  clickCount = 0;
   bvContentAuto: Observable<Baiviet[] | Observable<Baiviet[]>>;
   constructor(
     private apiService: ApiService,
@@ -117,6 +127,112 @@ export class LoadByCategoryComponent implements OnInit {
         }
       });
     });
+  }
+  clickLike(bv: BaiVietTotal, index: number) {
+    this.checkLogin();
+    // this.checkAlreadyLike(bv.idBV, index);
+    if (this.dataIDBV.length === 2) {
+      this.dataIDBV.splice(1, 1);
+      this.dataIDBV.unshift(bv.idBV);
+    } else {
+      this.dataIDBV.unshift(bv.idBV);
+    }
+    this.number1 = this.dataIDBV[0];
+    this.number2 = this.dataIDBV[1];
+    this.apiService.get('/api/notification/already-like/' + bv.idBV).subscribe(res => {
+      this.isLike = res;
+      let elem = document.getElementById('disableCateLike' + index);
+      if (this.isLike === true) {
+        elem.style.color = 'blue';
+      } else {
+        elem.style.color = 'black';
+      }
+      this.isActionLike = this.isLike;
+      if (this.isActionLike === true && this.number1 !== this.number2) {
+        $('#disableCateLike' + index).attr('disable', 'disable');
+
+        this.toastr.error('Bạn đã vote bài viết này rồi!');
+        // const bvp = {
+        //   luotthich: bv.luotthich - 1
+        // };
+        // this.apiService.post('/api/baiviet/like/' + bv.idBV, bvp).subscribe(res => {
+        //   this.getAllTopic();
+        //   this.disableDislike = true;
+        // });
+        this.isLike = null;
+        this.isDislike = null;
+      } else if (this.isActionLike === false && this.number1 !== this.number2) {
+        const bvp = {
+          luotthich: bv.luotthich + 1
+        };
+        this.apiService.post('/api/baiviet/like/' + bv.idBV, bvp).subscribe(res => {
+          this.getAllByCDid();
+          this.disableDislike = true;
+        });
+        this.isLike = null;
+        this.isDislike = null;
+      } else {
+        this.toastr.error('Bạn đã vote bài viết này rồi!');
+      }
+    });
+  }
+
+  clickDislike(bv: BaiVietTotal, index: number) {
+    this.checkLogin();
+    if (this.dataIDBV.length === 2) {
+      this.dataIDBV.splice(1);
+      this.dataIDBV.unshift(bv.idBV);
+    } else {
+      this.dataIDBV.unshift(bv.idBV);
+    }
+    this.number1 = this.dataIDBV[0];
+    this.number2 = this.dataIDBV[1];
+    // this.checkAlreadyDisLike(bv.idBV, index);
+    this.apiService.get('/api/notification/already-dislike/' + bv.idBV).subscribe(res => {
+      this.isDislike = res;
+      let elem = document.getElementById('disableCateDisLike' + index);
+      if (this.isDislike === true) {
+        elem.style.color = 'blue';
+      } else {
+        elem.style.color = 'black';
+      }
+      this.isActionDisLike = this.isDislike;
+      this.clickCount = index;
+      if (this.isActionDisLike === true && this.number1 !== this.number2) {
+        $('#disableCateDisLike' + index).attr('disable', 'disable');
+        this.toastr.error('Bạn đã vote bài viết này rồi!');
+        // const bvp = {
+        //   luotkhongthich: bv.luotkhongthich - 1
+        // };
+        // this.apiService.post('/api/baiviet/dislike/' + bv.idBV, bvp).subscribe(res => {
+        //   this.getAllTopic();
+        //   this.disableLike = true;
+        //   this.clickCount = 0;
+        // });
+        this.isDislike = null;
+        this.isLike = null;
+      } else if (this.isActionDisLike === false && this.number1 !== this.number2) {
+        const bvp = {
+          luotkhongthich: bv.luotkhongthich + 1
+        };
+        this.apiService.post('/api/baiviet/dislike/' + bv.idBV, bvp).subscribe(res => {
+          this.getAllByCDid();
+          this.disableLike = true;
+          this.clickCount = 0;
+        });
+        this.isDislike = null;
+        this.isLike = null;
+
+      } else {
+        this.toastr.error('Bạn đã vote bài viết này rồi!');
+      }
+    });
+  }
+  checkLogin() {
+    if (localStorage.getItem(TOKEN) === null) {
+      this.toastr.error('Bạn cần phải đăng nhập để thực hiện thao tác này!');
+      return;
+    }
   }
 
   onSend() {
